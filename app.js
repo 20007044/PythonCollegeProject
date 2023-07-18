8af07019da2aa839db551116d45050184b068dc4
cherukurinaveen036 <cherukurinaveen036@gmail.com>
2023-07-18 17:50:17 +0100

    databaselinked

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
+
+const host='0.0.0.0';
+mongoose.connect('mongodb+srv://akshaygupta42:Coldblot%4070@cluster0.se5soo7.mongodb.net/', { dbName: 'StudentInformationSystem' }).then('Connected database').catch(e => console.log(e));
