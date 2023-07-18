c6c39c60912b7e4d21c3aa7cb1ebd1da23bfc693
cherukurinaveen036 <cherukurinaveen036@gmail.com>
2023-07-18 18:53:13 +0100

    comment added

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const app=express();
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 
+
+//host for mongodb and database name and connectivity
 const host='0.0.0.0';
 mongoose.connect('mongodb+srv://akshaygupta42:Coldblot%4070@cluster0.se5soo7.mongodb.net/', { dbName: 'StudentInformationSystem' }).then('Connected database').catch(e => console.log(e));
