1f5993a83810e0f80245f0fd799834b8415d16f4
cherukurinaveen036 <cherukurinaveen036@gmail.com>
2023-07-18 20:14:58 +0100

    schemacreated

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const dotenv=require('dotenv');
 //host for mongodb and database name and connectivity
 const host='0.0.0.0';
 mongoose.connect('mongodb+srv://akshaygupta42:Coldblot%4070@cluster0.se5soo7.mongodb.net/', { dbName: 'StudentInformationSystem' }).then('Connected database').catch(e => console.log(e));
+
+
+const schema = mongoose.Schema(
+    {
+        ID: Number,
+        Name: String,
+        Roll: String,
+        Father: String,
+        Address: String,
+        Phone: String,
+    }
+);
